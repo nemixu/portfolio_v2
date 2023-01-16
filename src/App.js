15f6c2113f18c0b70533380f9a665f0ceef25588
nemixu <s.r.seagrave@gmail.com>
2023-01-16 16:41:09 +0000

    add framermotion to main app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import NavigationBar from "./components/NavigationBar";
 import About from "./components/About";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <div>
-      <NavigationBar />
-      <Header />
-      <About />
-      <Projects />
+      <motion.div>
+        <NavigationBar />
+        <Header />
+        <About />
+        <Projects />
+        <Contact />
+      </motion.div>
     </div>
   );
 }
