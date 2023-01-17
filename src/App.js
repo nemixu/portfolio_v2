d6b09212c3f61773e01fb84bde79a525e1142cc8
nemixu <s.r.seagrave@gmail.com>
2023-01-17 22:05:18 +0000

    created a jobs section, imported fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import "./fonts/Calibre/Calibre-Regular.ttf";
 import NavigationBar from "./components/NavigationBar";
 import About from "./components/About";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Jobs from "./components/Jobs";
 import { motion } from "framer-motion";
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
         <NavigationBar />
         <Header />
         <About />
+        <Jobs />
         <Projects />
         <Contact />
       </motion.div>
