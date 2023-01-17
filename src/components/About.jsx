37b1fd73a729bfbece762ef5ab5f7cf1f68efac7
nemixu <s.r.seagrave@gmail.com>
2023-01-17 22:05:48 +0000

    created an about section -may change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,8 @@ import { React, useState } from "react";
 import "../styles/AboutStyles.css";
 import { FaReact } from "react-icons/fa";
 import {
-  DiHtml5,
   DiJsBadge,
-  DiPython,
   DiDjango,
-  DiCss3,
   DiNetmagazine,
   DiBootstrap,
 } from "react-icons/di";
@@ -15,6 +12,39 @@ import { motion } from "framer-motion";
 
 function About() {
   const [inView, setInView] = useState(false);
+
+  const tech = [
+    {
+      technology: "React",
+      icon: FaReact,
+      Usage:
+        "I recently began learning React around the Christmas season, dedicating my time to building personal projects and actively seeking out new knowledge to enhance my understanding of the technology.",
+    },
+    {
+      technology: ".Net",
+      icon: DiNetmagazine,
+      Usage:
+        "When I secured an internship with Oneview, I had no prior experience with C# or Xamarin Forms. However, through on-the-job training and self-study, I was able to quickly acquire the necessary skills and eventually progressed with a promotion to become a front-end Xamarin software engineer.",
+    },
+    {
+      technology: "Django",
+      icon: DiDjango,
+      Usage:
+        "Django was the first web development framework I ever learned and used. I had the opportunity to apply my knowledge during college when I completed a major project, an e-commerce web application, using Django. The application featured Stripe payments, user profiles, and guest checkouts.",
+    },
+    {
+      technology: "JavaScript",
+      icon: DiJsBadge,
+      Usage:
+        "JavaScript has been a part of my toolset for some time, although it has not been my primary technology. I am motivated to improve my proficiency in it. I have applied JavaScript to various personal projects and also use it occasionally in my daily job.",
+    },
+    {
+      technology: "Bootstrap",
+      icon: DiBootstrap,
+      Usage:
+        "Bootstrap was the first CSS library I ever learned and has been a staple in almost all of my personal projects. I appreciate its ease of use and how quickly it allows me to create visually appealing applications with its built-in styling and features. Recently, I have been exploring the newest version of the library.",
+    },
+  ];
   return (
     <section className="text-white about-section" id="about">
       <motion.div
@@ -43,16 +73,13 @@ function About() {
           technology.
         </p>
         <h2 className="text-center text-white">Technologies I use</h2>
-        <span className="font-size d-flex text-center justify-content-center">
-          <FaReact />
-          <DiHtml5 />
-          <DiCss3 />
-          <DiJsBadge />
-          <DiNetmagazine />
-          <DiPython />
-          <DiDjango />
-          <DiBootstrap />
-        </span>
+        {tech.map((techUsed, i) => (
+          <div key={i}>
+            <div>{techUsed.technology}</div>
+            <div>{techUsed.Usage}</div>
+            <techUsed.icon className="icons" />
+          </div>
+        ))}
       </motion.div>
     </section>
   );
