393aa9b13cbeb6f457dfb83ab0c51a7a97d8e706
nemixu <s.r.seagrave@gmail.com>
2023-01-25 11:16:38 +0000

    Add structure to project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,89 +1,122 @@
 import React from "react";
 import "../styles/ProjectsStyles.css";
-
+import { FiGithub } from "react-icons/fi";
+import { IoMdOpen } from "react-icons/io";
 function Projects() {
   const projects = [
     {
       title: "DN Fitness",
       image:
-        "https://stevo-portfolio.s3.eu-west-1.amazonaws.com/static/images/dn-fitness.png",
+        "https://stevo-portfolio.s3.eu-west-1.amazonaws.com/static/images/dn-img.png",
       github: "https://github.com/nemixu/Milestone4",
-      deployed: "dn-fitness.onrender.com/",
-      technologies: ["Django", "python", "html", "css", "javascript", "stripe"],
+      deployed: "https://dn-fitness.onrender.com/",
+      description:
+        "An e-commerce web application built using the Django framework. It includes features such as integration with Stripe for payment processing at checkout, a custom shopping cart, a checkout page, user profiles, guest checkout options, and a specially designed navigation sidebar for mobile devices.",
+      technologies: ["Django", "Python", "HTML", "CSS", "JavaScript", "Stripe"],
     },
     {
       title: "Trivia Rooms",
       image:
-        "https://stevo-portfolio.s3.eu-west-1.amazonaws.com/static/images/triviarooms.png",
+        "https://stevo-portfolio.s3.eu-west-1.amazonaws.com/static/images/tr-img.png",
       github: "https://github.com/nemixu/trivia-rooms",
       deployed: "https://nemixu.github.io/trivia-rooms/",
-      technologies: ["Html", "css", "javascript", "bootstrap"],
+      description:
+        "My first JavaScript application is an interactive trivia game that utilizes the DOM to respond to user input. The game pulls questions and answers from an API, keeps track of the user's correct and incorrect answers, provides instant feedback to the user by highlighting the answers in green or red depending on if they are correct or incorrect, and finally displays the final score at the end.",
+      technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
     },
     {
       title: "Pokemon app",
       image:
-        "https://stevo-portfolio.s3.eu-west-1.amazonaws.com/static/images/Pokemon.JPG",
+        "https://stevo-portfolio.s3.eu-west-1.amazonaws.com/static/images/pm-img.png",
       github: "https://github.com/nemixu/pokemon-app",
       deployed: "https://nemixu.github.io/pokemon-app?offset=0",
-      technologies: ["React", "bootstrap"],
+      description:
+        "My first React application is a web app that utilizes the Pokemon API and Axios library to fetch information on all Pokemon. It features a custom Pokemon card component that displays each Pokemon's name, image, and associated stats, pagination functionality to fetch the next set of Pokemon, and a Pokemon detail page that provides further information on a selected Pokemon when clicked",
+      technologies: ["React", "Bootstrap"],
     },
     {
       title: "Movies Manager",
       image:
-        "https://stevo-portfolio.s3.eu-west-1.amazonaws.com/static/images/movies-manager.png",
+        "https://stevo-portfolio.s3.eu-west-1.amazonaws.com/static/images/mm-img.png",
       github: "https://github.com/nemixu/Movies-Manager",
       deployed: "https://movies-manager-twad.onrender.com/",
-      technologies: ["Python", "flask", "bootstrap"],
+      description:
+        "Movies Manager is a web application built using Flask, Python, JavaScript, and MongoDB. The goal of the project is to provide a platform for users to search for movies, create an account and store their favorite movies to their profile, it allows for CRUD functionality on their list of favorite movies.",
+      technologies: ["Python", "Flask", "Bootstrap"],
     },
     {
       title: "Giphy App",
       image:
-        "https://stevo-portfolio.s3.amazonaws.com/static/images/gifapp.PNG",
+        "https://stevo-portfolio.s3.eu-west-1.amazonaws.com/static/images/gy-img.png",
       github: "https://github.com/nemixu/giphy-app-2",
       deployed: "https://nemixu.github.io/giphy-app-2/",
-      technologies: ["Javascript"],
+      description:
+        "Giphy App is a JavaScript application that was built to practice JavaScript skills. It fetches gifs from the Giphy API, a user can type in a keyword and click search, the application will display a list of gifs that match the keyword in order on the page.",
+      technologies: ["JavaScript"],
     },
     {
       title: "Fibromyalgia Ireland",
-      image: "https://stevo-portfolio.s3.amazonaws.com/static/images/fibro.png",
+      image:
+        "https://stevo-portfolio.s3.eu-west-1.amazonaws.com/static/images/fm-img.png",
       github: "https://github.com/nemixu/Milestone1",
       deployed: "https://www.fibromyalgia.ie/",
-      technologies: ["Html", "css", "bootstrap"],
+      description:
+        "Fibromyalgia Ireland is my first web application that I built using HTML, CSS, and Bootstrap. This was my first project for university and I scored 98% on it. The goal was to create a single-page, static application that had clear sections and good contrast. It passed all Lighthouse accessibility testing.",
+      technologies: ["HTML", "CSS", "Bootstrap"],
     },
   ];
 
   return (
-    <div
+    <section
       className="projects-container text-center justify-content-center"
       id="work"
     >
-      <h2 className="text-center text-white container-fluid">My Projects</h2>
-      <div className="row">
-        {projects.map((project, index) => (
-          <div className="text-white col-sm-12 col-md-6" key={index}>
-            <div className="d-flex flex-column align-items-center p-5">
-              <h4>{project.title}</h4>
-              <img
-                className="project-image"
-                src={project.image}
-                alt={project.title}
-              />
-              <a href={project.github}>Repository</a>
-              <a href={project.deployed}>Deployed Site</a>
-              <div className="tech-container d-inline-flex">
-                {project.technologies.map((tech, idx) => {
-                  return (
-                    <p className="m-1" key={idx}>
-                      {tech}
-                    </p>
-                  );
-                })}
+      <h2 className="text-center heading-color fz-heading container-fluid">
+        Some Things I've Built
+      </h2>
+      <ul className="projects-outter-grid">
+        {projects.map((project, index) => {
+          const { image, title, github, deployed, description } = project;
+
+          return (
+            <li className="projects-inner-grid" key={index}>
+              <div className="project-content">
+                <div>
+                  <h3 className="project-title">
+                    <a href={deployed}>{title}</a>
+                  </h3>
+
+                  <div className="project-description">{description}</div>
+                  {project.technologies.map((tech, idx) => {
+                    return (
+                      <p
+                        className="d-inline-flex text-capitalize px-1 tech-text pt-3"
+                        key={idx}
+                      >
+                        {tech}
+                      </p>
+                    );
+                  })}
+
+                  <div className="project-links">
+                    <a target="_blank" href={github}>
+                      <FiGithub />
+                    </a>
+                    <a target="_blank" href={deployed}>
+                      <IoMdOpen />
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+
+              <div className="project-image">
+                <img className="img" src={image} />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 }
 
