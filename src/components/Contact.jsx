5cd424ed5864e89c1d5c5ca495f3de350da07a3e
nemixu <s.r.seagrave@gmail.com>
2023-02-14 12:09:28 +0000

    fix mis spelling of attribute

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,18 +4,18 @@ import "../styles/ContactStyles.css";
 function Contact() {
   return (
     <section id="contact" className="pt-5 mb-5">
-      <div class="container-fluid contact-container">
-        <div class="row">
-          <div class="col-12">
+      <div className="container-fluid contact-container">
+        <div className="row">
+          <div className="col-12">
             <h2 className="heading-color fz-md text-center font-weight-bold pb-3">
               What's Next?
             </h2>
-            <h2 class="fz-heading text-center font-weight-bold pb-3 heading-sizing contact-heading">
+            <h2 className="fz-heading text-center font-weight-bold pb-3 heading-sizing contact-heading">
               Get In Touch
             </h2>
           </div>
-          <div class="col-12 text-center">
-            <p class="mx-5 text-left pb-3 intro-paragraph text-white">
+          <div className="col-12 text-center">
+            <p className="mx-5 text-left pb-3 intro-paragraph text-white">
               I am always open to new opportunities in the software engineering
               field and am eager to collaborate on exciting projects. If you
               have a project in mind or are looking for a skilled software
@@ -25,7 +25,7 @@ function Contact() {
             <a
               rel="noopener"
               href="mailto:s.r.seagrave@gmail.com"
-              class="button-main text-white mr-3 text-center"
+              className="button-main text-white mr-3 text-center"
             >
               Say Hi!
             </a>
